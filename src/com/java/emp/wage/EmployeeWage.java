6f22197a8384a7334dc41f280b5737aa472864e0
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2023-01-11 08:30:42 +0530

    Added:  aily wage logic

--- src/com/java/emp/wage/EmployeeWage.java
+++ src/com/java/emp/wage/EmployeeWage.java
@@ -7,31 +7,30 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee\r\n" + "Wage Computation\r\n" + "Program on Master Branch");
-		
-		Random random  = new Random();
+
+		Random random = new Random();
 		int randomNum = random.nextInt(3);
-	
+
 		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int halfDayHour = 4;
 		int dailyWage = 0;
-		
-		
-		if(randomNum == 1)
-		{
+
+		switch (randomNum) {
+		case 1:
 			System.out.println("Employee worked fulltime");
 			dailyWage = fullDayHour * wagePerHour;
-		}
-		else if(randomNum ==  2)
-		{
+			break;
+		case 2:
 			System.out.println("Employee worked parttime");
 			dailyWage = halfDayHour * wagePerHour;
-		}
-		else
+			break;
+
+		default:
 			System.out.println("Employee is absent");
-		
-		System.out.println("Daily wage: "+dailyWage);
-	
+		}
+		System.out.println("Daily wage: " + dailyWage);
+
 	}
 
 }
